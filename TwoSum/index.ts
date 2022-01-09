efee5fd4bf03cce5b923663ab8876259944d5113
d.lomov <kqe4G5GU>
2022-01-09 16:20:11 +0300

    the simplest and most direct solution

--- TwoSum/index.ts
+++ TwoSum/index.ts
@@ -1,6 +1,6 @@
 const twoSum = (numbers: number[], target: number): number[] => {
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = 1; j < numbers.length; j++) {
+    for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === target) {
         return [i, j];
       }

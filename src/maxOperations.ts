a8038942b967157da4f0750b40b53d770d3599e0
d.lomov <d.lomov@s7.ru>
2023-05-08 19:52:04 +0300

    removed console.log's

--- src/maxOperations.ts
+++ src/maxOperations.ts
@@ -2,12 +2,10 @@ const maxOperations = (nums: number[], k: number): number => {
     if (!nums.length) return 0;
     let operationsCount = 0;
     
-    console.log('count', operationsCount)
-    
     const numbers = [...nums];
     const difference = k - numbers[0];
     const termIndex = numbers.findIndex((num, index) => num === difference && index !== 0);
-    console.log(numbers)
+    
     if (termIndex !== -1) {
         operationsCount += 1;
         numbers.splice(termIndex, 1);
@@ -19,8 +17,3 @@ const maxOperations = (nums: number[], k: number): number => {
     
     return operationsCount;
 }
-
-
-// console.log('result k = 6 ==', maxOperations([3, 1, 3, 4, 3], 6));
-// console.log('result k = 5 == ', maxOperations([1, 2, 3, 4], 5));
-console.log('result k = 10 == ', maxOperations([5, 5, 1, 3, 7, 9, 2, 4, 3, 8], 10));

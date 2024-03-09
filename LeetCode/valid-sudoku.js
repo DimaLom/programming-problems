abbdf70a3e4c349a6f758f7f5fa1dd159640f9c2
DimaLom <dm1trylomov@yandex.ru>
2024-03-09 17:21:33 +0300

    valid-sudoku.js optimized

--- LeetCode/valid-sudoku.js
+++ LeetCode/valid-sudoku.js
@@ -1,57 +1,39 @@
-const limits = [[0, 2], [3, 5], [6, 8]];
-
-const between = (x, min, max) => x >= min && x <= max;
-const findLimits = (x) => limits.find(([s, e]) => between(x, s, e));
-
-const getAreaCoordinates = (x, y) => {
-    const lx = findLimits(x);
-    const ly = findLimits(y);
-    
-    return { lx, ly };
-};
-
 /**
  * @param {string[][]} board
  * @return {boolean}
  */
-
-function isValidSudoku(board) {
-    const map = new Map();
+const isValidSudoku = function(board) {
+    const size = 9;
     
-    for (let y = 0; y < board.length; y++) {
-        for (let x = 0; x < board[y].length; x++) {
-            const current = board[y][x];
-            if(!/\d/.test(current)) {
-                continue;
-            }
+    const rows = new Array(size);
+    const cols = new Array(size);
+    const square = new Array(size);
+    
+    for (let i = 0; i < size; i++) {
+        rows[i] = new Set();
+        cols[i] = new Set();
+        square[i] = new Set();
+    }
+    
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            const value = board[row][col];
+            if(value === '.') { continue; }
             
-            const coordinates = map.get(current);
-            const { lx, ly } = getAreaCoordinates(x, y);
+            const sqr = Math.floor(row / 3) * 3 + Math.floor(col / 3);
             
-            const hasDuplicate = coordinates?.some(([cx, cy]) => cx === x || cy === y || (
-                between(cx, lx[0], lx[1]) && between(cy, ly[0], ly[1])
-            ));
-            
-            if(hasDuplicate) {
+            if(
+                rows[row]?.has(value) ||
+                cols[col]?.has(value) ||
+                square[sqr].has(value)) {
                 return false;
             }
             
-            map.set(current, coordinates ? [...coordinates, [x, y]] : [[x, y]]);
+            rows[row].add(value);
+            cols[col].add(value);
+            square[sqr].add(value);
         }
     }
+    
     return true;
-}
-
-console.log(isValidSudoku(
-    [
-        ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-        ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-        ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-        ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-        ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-        ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-        ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-        ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-        ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-    ]),
-);
+};
